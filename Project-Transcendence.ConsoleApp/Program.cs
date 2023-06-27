6b4c6defdcd86e5ea23a1b1a35d1187226eb250b
Piotr Niewczas <piterniewczas@gmail.com>
2023-06-27 13:50:59 +0200

    Added new Main Menu

--- Project-Transcendence.ConsoleApp/Program.cs
+++ Project-Transcendence.ConsoleApp/Program.cs
@@ -1,2 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using Project_Transcendence.ConsoleApp;
+
+
+
+Game game = new Game();
+game.Start();
