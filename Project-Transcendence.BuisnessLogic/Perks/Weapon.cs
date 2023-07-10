29fe3ac8b3cddff49a50a2b5baa6346d92f588fe
Paula Migulska <paula9610.pm@gmail.com>
2023-07-10 19:25:33 +0200

    Implemented components of the Item class

---
+++ Project-Transcendence.BuisnessLogic/Perks/Weapon.cs
@@ -0,0 +1,24 @@
+﻿using Project_Transcendence.ConsoleApp.Perks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Project_Transcendence.BuisnessLogic.Perks
+{
+    public class Weapon : IItem, IWeapon
+    {
+        public Weapon() { }
+        public Weapon(string name, string description, int damage) 
+        { 
+            Name = name;
+            Description = description;
+            Damage = damage;
+        }
+
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public int Damage { get; set; }
+    }
+}
