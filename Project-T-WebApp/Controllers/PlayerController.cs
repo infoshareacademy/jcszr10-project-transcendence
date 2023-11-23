ec39704ddd6a0049a9b2e185d19f37c7bcbc5e97
Paula <paula9610.pm@gmail.com>
2023-11-23 18:45:51 +0100

    Modify a Create Character Controller

--- Project-T-WebApp/Controllers/PlayerController.cs
+++ Project-T-WebApp/Controllers/PlayerController.cs
@@ -1,9 +1,13 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using Project_Transcendence.BuisnessLogic.Services;
+using Project_Transcendence_Database.DataAccess;
+using Project_Transcendence_Database.Entities;
 
 namespace Project_T_WebApp.Controllers
 {
     public class PlayerController : Controller
+
     {
 
         private readonly IPlayerService _playerService;
@@ -15,17 +19,42 @@ namespace Project_T_WebApp.Controllers
 
 
         [HttpPost]
-        public async Task<IActionResult> Create(string name, int raceId, int classId)
+        public async Task<IActionResult> CreateCharacter(string characterName, int raceId, int classId, string carouselControl, string carouselControl2)
         {
-            var character = await _playerService.CreateCharacterAsync(name, raceId, classId);
+                characterName = Request.Form["characterName"];
+                classId = GetClassIdFromClassButton(carouselControl);
+                raceId = GetRacesIdFromRaceButton(carouselControl2);
 
-            if (character != null)
+
+                var newPlayerCharacter = await _playerService.CreateNewCharacterAsync(characterName, raceId, classId);
+
+
+            return View();
+        }
+        private int GetClassIdFromClassButton(string classButton)
+        {
+            classButton = Request.Form["carousel-control"];
+            return classButton switch
             {
-                
-                return View(character);
+                "button_a" => 1,
+                "button_b" => 2,
+                "button_c" => 3,
+                "button_d" => 4,
+                _ => 0,
+            };
+        }
 
-            }          
-            return View(character);
+        private int GetRacesIdFromRaceButton(string raceButton)
+        {
+            raceButton = Request.Form["carousel-control-2"];
+            return raceButton switch
+            {
+                "button_f" => 1,
+                "button_g" => 2,
+                "button_h" => 3,
+                "button_i" => 4,
+                _ => 0,
+            };
         }
     }
 }
